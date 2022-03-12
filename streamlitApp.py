65d70ca1a5021e0b1cc0d7849a7e172a92a02a23
KishorSubbarayalu <kishorsubbarayalu@gmail.com>
2022-03-12 11:17:43 -0400

    Add input widgets

--- streamlitApp.py
+++ streamlitApp.py
@@ -129,6 +129,38 @@ st.map(df_map)
 
 # ****************************************************************************
 
+st.subheader("Input Widgets:")
 
+if st.button("hit like!"):
+    st.write("you liked this content")
+else:
+    pass
+
+name = st.text_input("Name")
+st.write(name)
+
+address = st.text_area("Enter your address")
+st.write(address)
+
+st.date_input("enter a date")
+
+st.time_input("enter a time")
+
+if st.checkbox("Accept the T&C",value = True):
+    st.write("Thank you")
+
+v1 = st.radio("Colours",["r","g","b"],index = 1)
+v2 = st.selectbox("Colours",["r","g","b"],index = 0)
+
+st.write(v1,v2)
+
+v3 = st.multiselect("Colours",["r","g","b"])
+st.write(v3)
+
+st.slider("age",min_value = 18,max_value=60,value = 30,step = 2)
+
+st.number_input("numbers",min_value = 18,max_value=60,value = 30,step = 2)
+
+# ****************************************************************************
 
 
