a4178aa6e2481171ba994e5e1e96bf0adc153a6a
KishorSubbarayalu <kishorsubbarayalu@gmail.com>
2022-03-12 06:55:13 -0400

    Add/Write Text

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,8 +1,61 @@
+
 # Load packages
 import streamlit as st
+import pandas as pd
 
-# Set title of the application
-st.title("Hello! \n")
-st.title("Welcome to Streamlit ML application")
+# ****************************************************************************
 
+# Set title of the application ; Text - title
+
+title = "Welcome to Streamlit App"
+st.title(title)
+
+# Text - markdown
+
+mdtext = 'Streamlit is **_really_ cool :sunglasses:**.'
+st.markdown(mdtext)
+
+# Text - header
+
+head = 'Machine Learning'
+st.header(head)
+
+cc = """This application has popular supervised and unsupervised machine
+           learning alogorithms."""
+st.caption(cc)
+
+# Text - subheader
+
+st.subheader('Supervised')
+st.subheader('Un-Supervised')
+
+# Text - code
+code = '''def function():
+     print("Print Function")'''
+st.code(code, language='python')
+
+# Simple text
+st.text('Below is the latex expression')
+
+# Text - Latex expressions
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+     
+# ****************************************************************************
+
+num = 1234
+
+string = "I Live in Canada!"
+
+df = pd.DataFrame({
+     'X': [1, 2, 3, 4],
+     'Y': [10, 20, 30, 40],
+ })
+
+st.write(num, string, df)
+
+# ****************************************************************************
 
